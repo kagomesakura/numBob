8f130340e3043a5d4615cda209838887cc3cb811
Kagome Miller <kagomemiller@gmail.com>
2021-02-14 19:09:54 -0600

    Update numBob.py

--- numBob.py
+++ numBob.py
@@ -5,9 +5,7 @@ numBob = 0
 
 for i in range(len(s)):
     if s[i:i+3] == "bob":
-        #The reason this works is that range returns a list of numbers
-        #in order, so it will go 0, 1, 2, 3... the [i:i+3]
-        #part cuts out a section of the string based on the how far into the string it is.
+        ## Check the current char + the next three chars.
         numBob += 1
 
 print ('Number of times bob occurs is: ' + str(numBob))
